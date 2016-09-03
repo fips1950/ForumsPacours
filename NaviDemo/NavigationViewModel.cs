2a697f1d2743c200aa30e0347a6ad8077cfac404
Peter <Peter@FL-LAPTOP4>
2016-09-03 07:18:09 +0200

    Add project files.

---
+++ NaviDemo/NavigationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace NaviDemo
+{
+  class NavigationViewModel
+  {
+    public NavigationViewModel()
+    {
+      _helloWorld = new TextBlockSetter();
+    }
+
+    TextBlockSetter _helloWorld;
+    public TextBlockSetter helloWorld
+    {
+      get
+      {
+        return _helloWorld;
+      }
+      set
+      {
+        _helloWorld = value;
+      }
+    }
+
+    public ICommand NavigationCommand { get { return new RelayCommand(navigationCommand); } }
+
+    private void navigationCommand(object destination)
+    {
+      switch (destination.ToString())
+      {
+        case "SecondPage":
+          {
+            //CurrentViewModel = SecondPage; ///Navigiert zu "SecondPageView"
+            helloWorld.Counter++;
+            break;
+          }
+      }
+    }
+  }
+}
---
+++ NaviDemo/TextBlockSetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NaviDemo
+{
+  public class TextBlockSetter : INotifyPropertyChanged
+  {
+    // boiler-plate
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "") => 
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    protected bool SetField<T>(ref T field, T value, string propertyName)
+    {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+      field = value;
+      OnPropertyChanged(propertyName);
+      return true;
+    }
+
+    // props
+    private int _Counter;
+    public int Counter
+    {
+      get { return _Counter; }
+      set { SetField(ref _Counter, value, "Counter"); }
+    }
+  }
+}
